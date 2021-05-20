8e87976e7c2c0900008115e8f7a686e46290b57a
Toshitaa18 <80677671+Toshitaa18@users.noreply.github.com>
2021-05-20 23:57:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var firstname="Toshitaa";
+console.log( firstname);
+
+var lastname="Srivastava";
+console.log( lastname);
+
+var fullname=firstname.concat(lastname);
+console.log(fullname );
+
+
+
+
+var maxnum=Math.max(15,10);
+console.log(maxnum );
+
+var minnum=Math.min(7,100);
+console.log(minnum );
+
+
+
+var a = 30;
+var b = 15;
+
+
+var add = a + b;
+console.log("add");
